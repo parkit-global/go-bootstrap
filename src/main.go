8783c4da7dfb748e9b4c3a76b9bb22a3dd5df76d
Jason Lee <lwlee2608@gmail.com>
2024-04-08 23:01:09 +0800

    add more templates

--- src/main.go
+++ src/main.go
@@ -8,18 +8,23 @@ import (
 )
 
 type TemplateData struct {
-	AppName string
+	AppName    string
+	ModuleName string
+	GoVersion  string
 }
 
 func main() {
 	fmt.Println("Bootstrap")
 
-	outputFileName := "main.go"
-	generateFile(
-		outputFileName,
-		TemplateData{
-			AppName: "DemoApp",
-		})
+	data := TemplateData{
+		AppName:    "DemoApp",
+		ModuleName: "github.com/username/demoapp",
+		GoVersion:  "1.21",
+	}
+
+	generateFile("main.go", data)
+	generateFile("Makefile", data)
+	generateFile("go.mod", data)
 }
 
 func generateFile(outputFileName string, data TemplateData) {
---
+++ template/Makefile.tpl
@@ -0,0 +1,18 @@
+GO = $(shell which go 2>/dev/null)
+
+APP 			:= {{.AppName}}
+VERSION 		:= v0.1.0
+
+
+.PHONY: all build clean test
+
+all: clean build
+
+clean:
+	$(RM) -rf bin/*
+build:
+	$(GO) build -o bin/$(APP) src/*.go
+run:
+	$(GO) run src/*.go
+test:
+	$(GO) test -v ./...
